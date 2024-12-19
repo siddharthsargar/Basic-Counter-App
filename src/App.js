7e610b2c322d32eb9f99b0a689bfc8721a1839e2
Siddharth Sargar <789sargarsiddharth@gmail.com>
2024-12-19 10:50:54 +0530

    added code for components and redux

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import React from 'react'
+
+// function App() {
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+// export default App
+
+
+import React from 'react';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>React Redux Counter</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, reset } from '../redux/counterSlice';
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Counter: {count}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
+    </div>
+  );
+};
+
+export default Counter;
